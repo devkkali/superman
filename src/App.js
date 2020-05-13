30bc9c2a1d00d89aea7a3d383ae61105afa3d92b
Nitesh <niteshdevkota2016@gmail.com>
2020-05-13 17:46:28 +0545

    1st commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import Contact from './components/Contact';
+import Login from './components/Login';
+
+function App() {
+  return (
+   <div>
+     <Router>
+       <Navbar />
+      <Switch>
+       <Route exact path='/' component={Home}   />
+       <Route  path='/about' component={About}   />
+       <Route path='/contact' component={Contact} />
+       <Route path='/login' component={Login} />
+       </Switch> 
+     </Router>
+   </div>
+  );
+}
+
+export default App;
